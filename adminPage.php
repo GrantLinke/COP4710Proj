59202e1db76a0d05700f614e03f58f9d0edf71a8
Grant Linke <GLinke123@gmail.com>
2021-12-03 13:53:11 -0500

    Incomplete admin homepage

---
+++ adminPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Register account</title>
+	<h1>Register a new account</h1>
+</head>
+
+<body>
+	<form method="post" action="createAdmin.html">
+		<button type="submit" id="create">
+		<label for="create">Create new admin</label>
+	</form>
+	
+	<form method="post" action="deleteAdmin.html">
+		<button type="submit" id="delete">
+		<label for="delete">Delete existing admin</label>
+	</form>
+		
+	<a href="login.html">
+		<input type="submit" name="back" value="Back">
+	</a>
+</body>
+</html>
