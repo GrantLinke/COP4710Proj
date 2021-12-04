05e03527d297606b7ac982896da7e82dcda98801
Grant Linke <GLinke123@gmail.com>
2021-12-04 10:35:06 -0500

    adds login functionality to be tested

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-function userExists($email)
+function userExists($conn, $email)
 {
 	$sql = "SELECT * FROM staff where email = ?;";
 	$stmt = mysqli_stmt_init($conn);
@@ -40,7 +40,7 @@ function userExists($email)
 	mysqli_stmt_close($stmt2);
 }
 
-function createUser($email, $password, $role)
+function createUser($conn, $email, $password, $role)
 {
 	$sql = "INSERT INTO ? (email, password) VALUES (?, ?);";
 	$stmt = mysqli_stmt_init($conn);
@@ -59,20 +59,53 @@ function createUser($email, $password, $role)
 	exit();
 }
 
-function check_login($password, $email)
+function login($conn, $password, $email)
 {
-	$sql = "SELECT COUNT(*)
-	FROM Professors P, Staff S
-	WHERE P.email=? AND P.password=? OR S.email=? AND S.password=?"
-	$stmt = $conn->prepare($sql);
-	$stmt->bind_param("ssss", $email, $password, $email, $password);
-	$stmt->execute();
-	$result=$stmt->get_result();
-	if($result != 0){ return false; }
-	else{ return true; }
+	$sql = "SELECT * FROM staff WHERE email=? AND password=?;";
+	$stmt = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt, $sql)) {
+		header("location: login.html?error=stmtFailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt, "ss" $email, $password);
+	mysqli_stmt_execute($stmt);
+	$result = mysqli_stmt_get_result($stmt);
+
+	$sql2 = "SELECT * FROM professors WHERE email=? AND password=?;";
+	$stmt2 = mysqli_stmt_init($conn);
+	if (!mysqli_stmt_prepare($stmt2, $sql2)) {
+		header("location: login.html?error=stmtFailed");
+		exit();
+	}
+
+	mysqli_stmt_bind_param($stmt2, "ss" $email, $password);
+	mysqli_stmt_execute($stmt2);
+	$result2 = mysqli_stmt_get_result($stmt2);
+
+	$row = mysqli_fetch_assoc($result);	// row represents staff
+	$row2 = mysqli_fetch_assoc($result2); // row2 represents professors
+
+	if($row == null && $row2 == null){ 
+		echo "Incorrect username/password";
+		header("location: login.html?error=incorrectLogin");
+		exit();
+	}
+	else if($row){	// if we need more session variables we declare them here
+		session_start();
+		$_SESSION["email"] = $email;
+		header("location: adminPage.html");
+		exit();
+	}
+	else{
+		session_start();
+		$_SESSION["email"] = $email;
+		header("location: home.html");
+		exit();
+	}
 }
 
-function changePassword($email, $oldPW, $newPW, $confPW){
+function changePassword($conn, $email, $oldPW, $newPW, $confPW){
 	if($newPW != $confPW){
 		header("location: changePassword.html?error=passwordMismatch")
 		exit();
