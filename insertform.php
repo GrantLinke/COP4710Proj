dbe86042f0f47e0122d1de034e3690c2d51d47ef
Ege Topcuoglu <ss.ege95@gmail.com>
2021-12-04 17:37:49 -0500

    Working insertform.

--- insertform.php
+++ insertform.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html>
-  
+
 <head>
     <title>Insert Page page</title>
 </head>
-  
+
 <body>
     <center>
         <?php
         $servername = "localhost";
-        $username = "username";
-        $password = "password";
-        
+        $username = "root";
+        $password = "";
+        $dbname = "libDb";
+
         // Create connection
-        $conn = new mysqli($servername, $username, $password);
-          
-        // Check connection
-        if($conn === false){
-            die("ERROR: Could not connect. " 
-                . mysqli_connect_error());
+        $conn = new mysqli($servername, $username, $password,$dbname);
+
+        if ($conn -> connect_errno) {
+         echo "Failed to connect to MySQL: " . $conn -> connect_error;
+         exit();
         }
-          
         // Taking all values from the form data(input)
         $classname = $_REQUEST['classname'];
         $semester = $_REQUEST['semestername'];
@@ -29,22 +28,28 @@
         $edition =  $_REQUEST['edition'];
         $publisher = $_REQUEST['publisher'];
         $isbn = $_REQUEST['isbn'];
-          
+
         // Performing insert query execution
-        $sql = "INSERT INTO Form  VALUES ('$classname', 
+        $sql = "INSERT INTO form  VALUES ('$classname',
             '$semester','$isbn','$booktitle', '$author', '$edition', '$publisher')";
-          
+
         if(mysqli_query($conn, $sql)){
-            echo "<h3>Your form submitted succesfully</h3>"; 
+            echo "<h3>Your form submitted succesfully</h3>";
         } else{
-            echo "ERROR: Hush! Sorry $sql. " 
+            echo "ERROR: Hush! Sorry $sql. "
                 . mysqli_error($conn);
         }
-          
+
         // Close connection
         mysqli_close($conn);
         ?>
+
+    <div class="logout">
+		<a href="login.html">
+			<button type="button" id="logoutButton" class="buttons" onclick="logoutfunction"> Log Out </button>
+		</a>
+	</div>
     </center>
 </body>
-  
+
 </html>
