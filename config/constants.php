0e3fe581a18eb77f7fc2bbc42e42c58720a735c1
Tyler Brigalli <59288324+Chronetic@users.noreply.github.com>
2021-12-07 10:18:54 -0500

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -8,7 +8,7 @@
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'new');
+    define('DB_NAME', 'libDB');
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error($db)); //Database Connection
     $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error($db)); //SElecting Database
